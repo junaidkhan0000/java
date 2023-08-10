2977e42d50fbeddde5a22cd78472589ec837de55
junaidkhan0000 <141704817+junaidkhan0000@users.noreply.github.com>
2023-08-10 16:15:27 +0530

    css.java

---
+++ css.java
@@ -0,0 +1,13 @@
++<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="css.css" rel="stylesheet">
+</head>
+<body>
+    <h1 id="a">introduction to css</h1>
+    <h1 style="color:hsl(0, 100%, 50%);"" class="b c"><b>Introduction to Internal css</h1></b>
+</body>
+</html>
